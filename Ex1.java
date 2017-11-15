810d56225d49af9d457aff76727dd553a9455899
chandrabagalkote <chandrabhanu25@gmail.com>
2017-11-15 22:46:03 +0530

    add the first java program

---
+++ Ex1.java
@@ -0,0 +1,11 @@
+import java.lang.System;
+import java.lang.String;
+
+public class Ex1 {
+public static void main(String args[]) {
+for(int i=1;i<=10;i++)
+{
+System.out.println("Welcome to Java in Jenkins");
+}
+}
+}
